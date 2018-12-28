9b5e401c4bc6663a4e75cc8804a31dbf72402576
Jonatas Antunes <jotunes23@gmail.com>
2018-12-28 20:57:37 -0200

    desafio 29 app de carros

---
+++ car-app/dom.js
@@ -0,0 +1,94 @@
+(function(win, document) {
+    'use strict';
+
+    // Função construtora
+    function DOM(elements) {
+        // Verifica se o objeto passado por parametro é uma instancia de DOM
+        if(!(this instanceof DOM)) 
+            return new DOM(elements);
+        this.element = document.querySelectorAll(elements);
+    };
+
+    // isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
+    DOM.isArray = function isArray(param) {
+        return Object.prototype.toString.call(param) === '[object Array]';
+    };
+
+    DOM.isObject = function isObject(param) {
+        return Object.prototype.toString.call(param) === '[object Object]';
+    };
+
+    DOM.isFunction = function isFunction(param) {
+        return Object.prototype.toString.call(param) === '[object Function]';
+    };
+
+    DOM.isNumber = function isNumber(param) {
+        return Object.prototype.toString.call(param) === '[object Number]';
+    };
+
+    DOM.isString = function isString(param) {
+        return Object.prototype.toString.call(param) === '[object String]';
+    };
+
+    DOM.isBoolean = function isBoolean(param) {
+        return Object.prototype.toString.call(param) === '[object Boolean]';
+    };
+
+    DOM.isNull = function isNull(param) {
+        return Object.prototype.toString.call(param) === '[object Null]' || Object.prototype.toString.call(param) === '[object Undefined]';
+    };
+
+    // Função que vai construir o addEventListener
+    DOM.prototype.on = function on(eventType, callback) {
+        // A function de callback ira passar por todos os elementos do primeiro argumento 'this.element'
+        Array.prototype.forEach.call(this.element, function (element) {
+            element.addEventListener(eventType, callback, false);
+        });
+    };
+
+    DOM.prototype.off = function off(eventType, callback) {
+         // A function de callback ira passar por todos os elementos do primeiro argumento 'this.element'
+         Array.prototype.forEach.call(this.element, function (element) {
+            element.removeEventListener(eventType, callback, false);
+        });
+    };
+
+    DOM.prototype.get = function get(index) {
+        if(!index) {
+            return this.element[0];
+        } else {
+            return this.element[index];
+        }
+    };
+
+    DOM.prototype.forEach = function forEach() {
+        return Array.prototype.forEach.apply(this.element, arguments);
+    };
+
+    DOM.prototype.map = function map() {
+        return Array.prototype.map.apply(this.element, arguments);
+    };
+
+    DOM.prototype.filter = function filter() {
+        return Array.prototype.filter.apply(this.element, arguments);
+    };
+
+    DOM.prototype.reduce = function reduce() {
+        return Array.prototype.reduce.apply(this.element, arguments);
+    };
+
+    DOM.prototype.reduceRight = function reduceRight() {
+        return Array.prototype.reduceRight.apply(this.element, arguments);
+    };
+
+    DOM.prototype.every = function every() {
+        return Array.prototype.every.apply(this.element, arguments);
+    };
+
+    DOM.prototype.some = function some() {
+        return Array.prototype.some.apply(this.element, arguments);
+    };
+
+    win.DOM = DOM;
+
+})(window, document);
